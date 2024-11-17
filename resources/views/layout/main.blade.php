ac8206de86b4d97fece13b9c98e57f8f569d59d4
Yurisha Anindya <yurishaanindya154@gmail.com>
2024-11-17 21:00:44 +0700

    navigasi login awal

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -10,8 +10,9 @@
 </head>
 
 <body>
-    <div id="kiri" class="w-1/2 bg-green-600 h-screen border">
-        <h1>yurisha</h1>
+    <div class="bg-gray-100 border shadow mx-auto w-auto p-5 flex justify-end">
+        
+        <a href="/login" class="border border-green-700 text-white text-xl font-bold font-mono p-5 shadow bg-green-500 rounded-full hover:bg-green-600">Login Page</a>
     </div>
 </body>
 
